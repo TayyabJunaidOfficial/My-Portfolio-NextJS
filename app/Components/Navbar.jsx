4994cead41dde8a0133a34e1a5d5ca534fe70914
Tayyab Junaid <tayyabjunaidofficial@gmail.com>
2025-05-05 15:00:48 +0500

    Creating Navbar

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -1,18 +1,53 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { SiGithub } from "react-icons/si";
+import { SiLinkedin } from "react-icons/si";
+import { SiGmail } from "react-icons/si";
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between  items-center p-7'>
-        <div className='text-white text-2xl'>Tayyab J.</div>
-        <ul className='flex justify-center items-center gap-8'>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="About">About</Link></li>
-            <li><Link href="Tech_Stack">Tech Stack</Link></li>
-            <li><Link href="Contact">Contact</Link></li>
+    <nav className="flex justify-evenly items-center gap-96 p-7">
+      <h1 className="text-3xl font-bold bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent">
+        Tayyab J.
+      </h1>
+      <ul className="flex justify-center items-center gap-10">
+        <li>
+          <Link
+            className="hover:bg-gradient-to-r hover:from-purple-400 hover:via-pink-500 hover:to-red-500 hover:bg-clip-text hover:text-transparent transition duration-300"
+            href="/"
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="About">About</Link>
+        </li>
+        <li>
+          <Link href="Tech_Stack">Tech Stack</Link>
+        </li>
+        <li>
+          <Link href="Contact">Contact</Link>
+        </li>
+        <ul className="flex justify-center items-center gap-5">
+          <li>
+            <Link href="">
+              <SiGithub size={20} />
+            </Link>
+          </li>
+          <li>
+            <Link href="">
+              <SiLinkedin size={20} />
+            </Link>
+          </li>
+          <li>
+            <Link href="">
+              <SiGmail size={20} />
+            </Link>
+          </li>
         </ul>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
