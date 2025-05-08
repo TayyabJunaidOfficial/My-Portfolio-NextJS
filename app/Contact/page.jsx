03abf943a66f2430050060aea06ddfc4c8ee62dd
Tayyab Junaid <tayyabjunaidofficiall@gmail.com>
2025-05-08 20:03:06 +0500

    Started Working on Contact Page

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import Navbar from '../Components/Navbar/Navbar'
+import React from "react";
 
 const Contact = () => {
   return (
-      <div>
-        Contact
-        </div>
-  )
-}
+    <main className="flex flex-col md:justify-center items-center gap-8 py-5 bg-white text-black dark:bg-black dark:text-white">
+      <h1 className="sm:text-5xl text-4xl p-3 text-center gradient-text">
+        Contact Us
+      </h1>
+      <p>For any questions please cotact us:</p>
+      <p>
+        I’M ALWAYS OPEN TO DISCUSSING PRODUCT
+        <b>DESIGN WORK OR PARTNERSHIPS.</b>{" "}
+      </p>
+      <form action="">
+        <input type="name" />
+        <input type="email" />
+        <input type="message" />
+      </form>
+    </main>
+  );
+};
 
-export default Contact
+export default Contact;
