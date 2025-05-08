2f1e41cb88d9056cabb0fc0be1d8a592aa76a25d
Tayyab Junaid <tayyabjunaidofficiall@gmail.com>
2025-05-08 21:20:24 +0500

    Completed Working on Contact Page

---
+++ app/Components/Form/Form.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(5, { message: "Minimum 5 characters" })
+    .max(15, { message: "Maximum 15 characters" }),
+    email: z.string().email({ message: "Invalid email address" }),
+  message: z.string().min(10, { message: "Min 15 characters long" }),
+});
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    mode: "onBlur",
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (data) => console.log(data);
+  return (
+      <>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-4 w-full sm:max-w-lg max-w-sm"
+      >
+        <div className="flex justify-center items-center relative">
+          <input
+            id="name"
+            type="text"
+            placeholder=" "
+            {...register("name")}
+            className="peer sm:w-full w-xs border border-gray-500 rounded px-3 pt-6 pb-2 outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <label
+            htmlFor="name"
+            className="absolute sm:left-3 left-7 top-2 text-gray-500 text-sm transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-focus:top-2 peer-focus:text-sm"
+          >
+            Name
+          </label>
+          {errors.name && (
+            <span className="text-red-500 text-sm">{errors.name.message}</span>
+          )}
+        </div>
+
+        <div className="flex justify-center items-center relative">
+          <input
+            id="email"
+            type="email"
+            placeholder=" "
+            {...register("email")}
+            className="peer sm:w-full w-xs border border-gray-500 rounded px-3 pt-6 pb-2 outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <label
+            htmlFor="email"
+            className="absolute sm:left-3 left-7 top-2 text-gray-500 text-sm transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-focus:top-2 peer-focus:text-sm"
+          >
+            Email
+          </label>
+          {errors.email && (
+            <span className="text-red-500 text-sm">{errors.email.message}</span>
+          )}
+        </div>
+
+        <div className="flex justify-center items-center relative">
+          <textarea
+            id="message"
+            type="text"
+            placeholder=" "
+            {...register("message")}
+            className="peer sm:w-full w-xs border border-gray-500 rounded px-3 pt-6 pb-2 outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <label
+            htmlFor="message"
+            className="absolute sm:left-3 left-7 top-2 text-gray-500 text-sm transition-all peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-focus:top-2 peer-focus:text-sm"
+          >
+            Message
+          </label>
+          {errors.message && (
+            <span className="text-red-500 text-sm">
+              {errors.message.message}
+            </span>
+          )}
+        </div>
+
+        <div className="flex sm:justify-end justify-center">
+        <input
+          type="submit"
+          value="Send"
+          className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-white border-2 w-24 rounded-lg py-2 font-semibold transition hover:shadow-[0_0_15px_#ec4899]"
+          />
+        </div>
+      </form>
+      </>
+  )
+}
+
+export default Form
--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -1,21 +1,23 @@
+"use client";
 import React from "react";
+import Form from "../Components/Form/Form";
+
 
 const Contact = () => {
+
   return (
     <main className="flex flex-col md:justify-center items-center gap-8 py-5 bg-white text-black dark:bg-black dark:text-white">
       <h1 className="sm:text-5xl text-4xl p-2 text-center gradient-text">
         Contact Us
       </h1>
-      <p className="flex gap-2 justify-center items-center sm:text-xl text-lg text-center">
+      <p className="flex sm:flex-row flex-col gap-2 justify-center items-center sm:text-xl text-lg text-center">
         I’m Always Open To Discussing Product
-        <b className="gradient-text">Design Work Or Partnerships.</b>{" "}
+        <b className="gradient-text">Design Work Or Partnerships.</b>
       </p>
-      <p className="sm:text-xl text-lg text-center">For any questions please contact us:</p>
-      <form action="">
-        <label htmlFor="">Name:</label><input type="name" />
-        <label htmlFor="">Email:</label><input type="email" />
-        <label htmlFor="">Message</label><input type="message" />
-      </form>
+      <p className="text-xl text-center">
+        For any questions please contact us:
+      </p>
+      <Form/>
     </main>
   );
 };
