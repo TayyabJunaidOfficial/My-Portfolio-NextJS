43fbde8adf68ef423cf7cd1efbd1137352aaaf21
Tayyab Junaid <tayyabjunaidofficiall@gmail.com>
2025-05-18 07:51:45 +0500

    Trying to add some animations

--- app/Components/Cards/Exp_Card.jsx
+++ app/Components/Cards/Exp_Card.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { GiSuitcase } from "react-icons/gi";
+import { CardContainer, CardBody, CardItem } from "@/components/ui/3d-card";
+
 
 
 const Exp_Card = () => {
@@ -9,10 +11,27 @@ const Exp_Card = () => {
             <GiSuitcase className="text-green-600" />
             <p>Career Highlights</p>
           </div>
-          <p className="text-gray-600">
-            No formal work experience yet, but passionate and actively learning.
-            Eager to contribute and grow professionally.
-          </p>
+          <div className="flex flex-col gap-5 justify-center items-center text-center">
+                    <CardContainer className="inter-var">
+                      <CardBody
+                        className={`
+                          transition-all duration-300
+                          bg-white dark:bg-zinc-900
+                          text-black dark:text-white
+                          shadow-md rounded-2xl max-w-lg
+                          border border-transparent
+                          hover:border-gray-300 dark:hover:border-emerald-400/40
+                        `}
+                      >
+                        <CardItem translateZ={30} className="px-4 py-6">
+                          <p className="text-gray-600 dark:text-gray-300">
+                          No formal work experience yet, but passionate and actively learning. Eager to contribute and grow professionally.
+
+</p>
+                        </CardItem>
+                      </CardBody>
+                    </CardContainer>
+                </div>
     </section>
   )
 }
